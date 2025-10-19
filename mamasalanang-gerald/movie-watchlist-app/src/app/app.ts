2e9c9171d99d22f24c1fe560aae570f8739c2e05
mamasalanang-gerald <geraldkasan163@gmail.com>
2025-10-19 13:00:07 +0800

    Integrate movie form and list components in main app

--- mamasalanang-gerald/movie-watchlist-app/src/app/app.ts
+++ mamasalanang-gerald/movie-watchlist-app/src/app/app.ts
@@ -1,12 +1,20 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MovieForm, Movie } from './movie-form/movie-form';
+import { MovieList } from './movie-list/movie-list';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MovieForm, MovieList],
   templateUrl: './app.html',
   styleUrl: './app.sass'
 })
 export class App {
-  protected readonly title = signal('movie-watchlist-app');
+  protected readonly title = signal('Movie Watchlist App');
+  
+  movies: Movie[] = [];
+
+  onMovieAdded(movie: Movie) {
+    this.movies.push(movie);
+  }
 }
